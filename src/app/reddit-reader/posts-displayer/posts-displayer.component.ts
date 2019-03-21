115b1becee78c38d0e6de5278a19968b32786935
Guillaume Fiette <guillaume.fiette@gmail.com>
2019-03-21 16:16:25 +0100

    Picker - data reload

--- src/app/reddit-reader/posts-displayer/posts-displayer.component.ts
+++ src/app/reddit-reader/posts-displayer/posts-displayer.component.ts
@@ -18,15 +18,31 @@ class Post {
 })
 export class PostsDisplayerComponent implements OnInit {
 
-  @Input() subreddit : string
+  private _subreddit : string;
+
+  @Input() set subreddit(sub : string) {
+    this._subreddit = sub;
+    this.loadPosts(this.subreddit);
+  }
+
+  get subreddit() {
+    return this._subreddit;
+  }
 
   public posts : Post[] = []
 
   constructor(private http: HttpClient) { }
 
+  private loadPosts(subreddit) {
+    this.http.get(`https://www.reddit.com/r/${subreddit}/.json`)
+             .subscribe(
+               (data : any)  => this.posts = data.data.children.map(children => new Post(children.data.title, children.data.ups))
+             , (error : any) => { console.error(error); this.posts = [] } 
+           )
+  }
+
   ngOnInit() {
-    this.http.get(`https://www.reddit.com/r/${this.subreddit}/.json`)
-             .subscribe((data : any) => this.posts = data.data.children.map(children => new Post(children.data.title, children.data.ups)))
+    this.loadPosts(this.subreddit)
   }
 
 }
